5beda43b5824f15f312371a38a402515387ebb3d
Martino Yovo <martinosoyen@gmail.com>
2023-03-17 12:13:17 +0000

    Flower

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_logo_animation/custom_loader.dart';
+import 'package:flutter_logo_animation/flower.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CustomLoader()
+      home: const FlowerAnimation()
     );
   }
 }
